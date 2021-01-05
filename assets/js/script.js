656b917a5b5c50cd4553dd9efc33cb9332c8d315
Raymond Lopez <raymondlopez13@yahoo.com>
2021-01-04 18:34:55 -0600

    Edit task P element

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,8 +44,22 @@ var loadTasks = function() {
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
-
-
+$('.list-group').on('click','p', function() {
+  var text = $(this).text().trim();
+  var textInput = $('<textarea>').addClass('form-control').val(text);
+  $(this).replaceWith(textInput);
+  textInput.trigger('focus');
+  console.log(text);
+});
+$(".list-group").on('blur', 'textarea', function() {
+  var text = $(this).val().trim();
+  var status = $(this).closest('.list-group').attr('id').replace('list-', '');
+  var index = $(this).closest('list-group-item').index();
+  tasks[status][index].text = text;
+  saveTasks();
+  var taskP = $('<p>').addClass('m-1').text(text);
+  $(this).replaceWith(taskP);
+});
 
 
 // modal was triggered
